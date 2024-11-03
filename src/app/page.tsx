cb234752de3e42df4993a88fd00409f8e15dfbf5
hopeifeanyi1 <hopeifeanyi2@gmail.com>
2024-11-03 12:29:57 +0100

    welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="">
-      
+    <div className="flex items-center justify-center h-[90dvh]">
+      <div className="">
+        <p className="text-5xl text-center">Welcome</p>
+        <Link href='/home' className="text-center text-blue-600">Click Me to navigate to other pages</Link>
+      </div>
     </div>
   );
 }
