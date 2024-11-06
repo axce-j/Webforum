7d73632e7990b96477e258772dde2d5f6b594792
hopeifeanyi1 <hopeifeanyi2@gmail.com>
2024-11-06 14:03:18 +0100

    home page layout

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Home page
+    <div className='md:py-5 py-2'>
+      <p className='py-3 px-7 md:text-4xl text-3xl border-b md:border-[#7c3085] border-slate-400'>Companys Name</p>
+      <p className='md:text-3xl text-xl px-5 py-3 md:px-9 h-[80dvh]'>Recent Posts</p>
     </div>
   )
 }
