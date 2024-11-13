96ffcebce8cb153f5be120e85f9e2d4eacc71318
axce-j <johnobi699@gmail.com>
2024-11-13 03:45:16 +0000

    initial

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/",
+
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
